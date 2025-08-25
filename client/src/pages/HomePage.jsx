1e41ae8518e19d7e7014dc0f89d6dcaaa5d34fed
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-08-25 09:58:12 +0530

    installed assets

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import LeftSideBar from '../components/LeftSideBar'
+import ChatBox from '../components/ChatBox'
+import RightSideBar from '../components/RightSideBar'
 
 function HomePage() {
+
+  const [userSelected, setUserSelected] = useState(false)
+
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[15%]'>
-
+        <div className={`backdrop-blur-sm border-2 [border-color:aliceblue] rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative
+          ${ userSelected? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
+          <LeftSideBar />
+          <ChatBox />
+          <RightSideBar />
+        </div>
     </div>
   )
 }
