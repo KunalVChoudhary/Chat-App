14f7e811fe44c2bcc9609cf8d9f915763e10d0dd
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-08-26 09:46:33 +0530

    dropdown for profile edit and logout in leftsidebar

--- client/src/components/LeftSideBar.jsx
+++ client/src/components/LeftSideBar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import assets from '../assets/assets'
 
-function LeftSideBar() {
+function LeftSideBar({userSelected, setUserSelected}) {
+
+  const navigate = useNavigate()
+
   return (
-    <div>LeftSideBar</div>
+    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${userSelected? 'max-md:hidden' : ''} `}>
+      <div className="pb-5">
+        <div className="flex justify-between items-center">
+          <img src={assets.logo} alt="logo" className="max-w-40" />
+          <div className="relative py-2 group">
+            <img src={assets.menu_icon} alt="menu_icon" className="max-h-5 cursor-pointer" />
+            <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-grey-600 text-grey-100 hidden group-hover:block'>
+              <p onClick={()=>navigate('/profile')} className="cursor-pointer text-sm">Edit Profile</p>
+              <hr className="my-2 border-t border-gray-500" />
+              <p onClick={''} className="cursor-pointer text-sm">Log Out</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
