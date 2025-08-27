8d548dc260a4a7366eeef5a13c8c34c07c37499f
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-08-27 17:21:07 +0530

    passing props to child component

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -8,12 +8,12 @@ function HomePage() {
   const [userSelected, setUserSelected] = useState(false)
 
   return (
-    <div className='border w-full h-screen sm:px-[15%] sm:py-[15%]'>
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
         <div className={`backdrop-blur-sm border-2 [border-color:aliceblue] rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative
           ${ userSelected? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
-          <LeftSideBar />
-          <ChatBox />
-          <RightSideBar />
+          <LeftSideBar userSelected={userSelected} setUserSelected={setUserSelected} />
+          <ChatBox userSelected={userSelected} setUserSelected={setUserSelected}  />
+          <RightSideBar userSelected={userSelected} setUserSelected={setUserSelected}  />
         </div>
     </div>
   )
