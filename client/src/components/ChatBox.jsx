cd45a4985622eb066b11920136c15a0c4a380dc8
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-09-01 11:21:27 +0530

    added input field to the chat box

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import assets, { messagesDummyData } from '../assets/assets'
+import { formatMessageTime } from '../lib/utils'
 
 function ChatBox({userSelected, setUserSelected}) {
 
@@ -42,12 +43,25 @@ function ChatBox({userSelected, setUserSelected}) {
           }
           <div className="text-center text-xs">
             <img src={msg.senderId=== '680f50e4f10f3cd28382ecf9'  ? assets.avatar_icon : assets.profile_martin} alt="" className='rounded-full w-7' />
-            <p className='text-gray-500'>{msg.createdAt}</p>
+            <p className='text-gray-500'>{formatMessageTime(msg.createdAt)}</p>
           </div>
           </div>)
         })}
         <div ref={scrollEnd}></div>
       </div>
+
+      {/* input area */}
+      <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
+        <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
+          <input type="text" placeholder='Send a message' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400' />
+          <input type="file" id="image" accept='image/png,image/jpeg' hidden  />
+          <label htmlFor="image">
+              <img src={assets.gallery_icon} alt="input image" className='w-5 mr-2 cursor-pointer' />
+          </label>
+          <img src={assets.send_button} alt="" className="w-7 cursor-pointer" />
+        </div>
+      </div>
+
     </div>
   )
   :(
