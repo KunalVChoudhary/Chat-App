ec000a113b51aa94e88733ed83907c88e6f855a2
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-09-07 14:49:38 +0530

    created middleware for protect routes

---
+++ server/middleware/auth.js
@@ -0,0 +1,22 @@
+import User from "../models/User";
+
+//Middleware to protect routes
+export const protectRoute = async (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.id).select("-password");
+
+        if (!user) {
+            return res.status(401).json({ success: false, message: "Unauthorized" });
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(401).json({ success: false, message: error.message });
+    }
+};
