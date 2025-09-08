90e2666064a7e9c1933d3e04156eaec625893a5d
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-09-08 22:32:03 +0530

    created controller function to get messages between 2 users and update the seen value in the mongodb document

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,3 +1,5 @@
+import Message from "../models/Message";
+
 //Get all users except the loggenin user
 export const getUserForSidebar = async (req, res) => {
     try {
@@ -10,6 +12,26 @@ export const getUserForSidebar = async (req, res) => {
             return { ...user.toObject(), unseenCount };
         }));
         res.status(200).json({ success: true, users: filteredUsers, usersWithUnseenCount });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+//get all messages for between 2 user
+export const getMessagesBetweenUsers = async (req, res) => {
+    try {
+        const userID= req.user._id
+        const otherUserId = req.params.userId;
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: userID, receiverId: otherUserId },
+                { senderId: otherUserId, receiverId: userID }
+            ]
+        }).sort({ createdAt: 1 }); // Sort by creation time in ascending order
+        await Message.updateMany({ senderId: otherUserId, receiverId: userID, seen: false }, { $set: { seen: true } });
+        res.status(200).json({ success: true, messages });
     } catch (error) {
         console.error(error.message);
         res.status(500).json({ success: false, message: error.message });
