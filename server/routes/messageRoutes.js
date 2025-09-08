61bfd34f38e2ee57fa09bb9196e6484b80d47e18
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-09-08 23:38:33 +0530

    created messgae routes

---
+++ server/routes/messageRoutes.js
@@ -0,0 +1,11 @@
+import {Router} from 'express';
+import {protectRoute} from '../middleware/auth.js';
+import {getUserForSidebar, getMessagesBetweenUsers, markMessagesAsSeen} from '../controllers/messageController.js';
+
+const messageRouter = Router();
+
+messageRouter.get('/sidebar-users', protectRoute, getUserForSidebar);
+messageRouter.get('/messages/:userId', protectRoute, getMessagesBetweenUsers);
+messageRouter.put('/mark-seen/:messageId', protectRoute, markMessagesAsSeen);   
+
+export default messageRouter;
