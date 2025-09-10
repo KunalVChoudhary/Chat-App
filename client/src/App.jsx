1f15781d832b404b51b5f01fe8531d4765ca07e0
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-09-10 15:06:52 +0530

    using contextapi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import {Route, Routes} from 'react-router-dom'
+import {useContext} from 'react'
+import {Navigate, Route, Routes} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
+import { AuthContext } from '../context/authContext'
 
 function App() {
+  const {authUser} = useContext(AuthContext)
   return (
     <div className="bg-[url('./assets/bgimage.png')] bg-cover">
       <Routes>
 
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/login' element={<LoginPage/>} />
-        <Route path='/profile' element={<ProfilePage/>} />
+        <Route path='/' element={authUser ? <HomePage/> : <Navigate to="/login" />} />
+        <Route path='/login' element={authUser ? <Navigate to="/" /> : <LoginPage />} />
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
 
       </Routes>
     </div>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,16 @@
-import {  } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from '../context/authContext'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <ToastContainer position="top-right" />
+      <App />  
+    </AuthProvider>
   </BrowserRouter>,
 )
